6174a4441b7d79490250733be01d013abcecc3ca
Sanna <rakeshkumarreddy907@gmail.com>
2018-11-04 14:11:32 +0530

    formbuilderchanges

---
+++ src/app/Create/create-comp-using-reactive-form/create-comp-using-reactive-form.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-create-comp-using-reactive-form',
+  templateUrl: './create-comp-using-reactive-form.component.html',
+  styleUrls: ['./create-comp-using-reactive-form.component.css']
+})
+export class CreateCompUsingReactiveFormComponent implements OnInit {
+
+  employeeForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+
+  }
+
+  ngOnInit() {
+    this.employeeForm = this.fb.group({
+      firstname: [''],
+      lastname: [''],
+      skills: this.fb.group({
+        skillName: [''],
+        experience: [''],
+        proficiency:['']
+      })
+    });
+  }
+
+  onClickSubmitButton(){
+ console.log( this.employeeForm.get('firstname').value);
+}
+  
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { RecaptchaModule, RecaptchaSettings, RECAPTCHA_SETTINGS } from 'ng-recaptcha';
 import { RecaptchaFormsModule } from 'ng-recaptcha/forms';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CreateComponentComponent } from './Create/create-component/create-component.component';
 import { CustomValidatorDirective } from './Create/date-custom-validator';
+import { CreateCompUsingReactiveFormComponent } from './Create/create-comp-using-reactive-form/create-comp-using-reactive-form.component';
+import { PageNotFoundComponent } from './Error/page-not-found.component';
+
+const approutes: Routes = [
+    { path: 'createtemplateform', component: CreateComponentComponent },
+    { path: 'createreactiveform', component: CreateCompUsingReactiveFormComponent },
+    {path:'',redirectTo:'/createreactiveform',pathMatch: 'full'},
+    { path: '**', component: PageNotFoundComponent }];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CreateComponentComponent,
-    CustomValidatorDirective  
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    BsDatepickerModule.forRoot(),
-    RecaptchaModule.forRoot(),
-    RecaptchaFormsModule,
-    HttpClientModule
-  ],
-  providers: [{
-    provide: RECAPTCHA_SETTINGS,
-    useValue: { siteKey: '6Lcff3gUAAAAAKrYHorQA1tPQdLBXSM-pLN3jfyk' } as RecaptchaSettings,
-  }],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        CreateComponentComponent,
+        CustomValidatorDirective,
+        CreateCompUsingReactiveFormComponent,
+        PageNotFoundComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        BsDatepickerModule.forRoot(),
+        RecaptchaModule.forRoot(),
+        RecaptchaFormsModule,
+        HttpClientModule,
+        RouterModule.forRoot(approutes),
+        ReactiveFormsModule
+    ],
+    providers: [{
+        provide: RECAPTCHA_SETTINGS,
+        useValue: { siteKey: '6Lcff3gUAAAAAKrYHorQA1tPQdLBXSM-pLN3jfyk' } as RecaptchaSettings,
+    }],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
